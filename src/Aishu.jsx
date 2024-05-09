a3b8a544371aa6ea901f70181b34eeb99ab7468f
aishwariya <1312aishwariya@gmail.com>
2024-05-09 22:36:50 +0530

    new project

---
+++ src/Aishu.jsx
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Aishu() {
+  return (
+    <div>poda patti</div>
+  );
+}
+
+export default Aishu;
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Aishu from './Aishu';
+
+function App() {
+  return (
+    <div>
+      <Aishu/>
+    </div>
+
+  );
+}
+
+export default App;
